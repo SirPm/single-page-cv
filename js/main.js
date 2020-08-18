28714d61763cb69e0e0a80d4b3743cf13eda380c
SirPm <odumosuoluwapm@gmail.com>
2020-08-18 07:34:09 +0100

    nav bar animation done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const menuBtn = document.getElementById('hamburger');
+/*const menuBtn = document.getElementById('hamburger');
 nav = document.getElementById('menuLinks');
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -6,4 +6,29 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
     nav.classList.toggle('hidden');
     console.log(nav)
-};
+};*/
+
+navSlide = () => {
+    const nav = document.querySelector('.nav-links');
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle nav
+        nav.classList.toggle('nav-active');
+
+        // Animate links
+        navLinks.forEach( (link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+            }
+        });
+
+        // Animate the burger button
+        burger.classList.toggle('toggleBurger');
+    });
+}
+
+navSlide();
